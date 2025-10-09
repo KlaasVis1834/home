8f1e8417501a0455ed98854530a3e770741d1a27
KlaasVis1834 <info@klaasvis.nl>
2025-10-09 15:54:58 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const hamburger = document.querySelector('.hamburger');
+    const navList = document.querySelector('.nav-list');
+
+    hamburger.addEventListener('click', () => {
+        navList.classList.toggle('active');
+        hamburger.textContent = navList.classList.contains('active') ? '✕' : '☰';
+    });
+});
