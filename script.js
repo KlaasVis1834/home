8745bf7858654fb4001f0780159322186902f3cd
KlaasVis1834 <info@klaasvis.nl>
2025-10-15 22:01:24 +0200

    Update script.js

--- script.js
+++ script.js
@@ -83,8 +83,34 @@ document.addEventListener("DOMContentLoaded", () => {
 
     submitBtn.disabled = false;
     submitBtn.textContent = "Verstuur Bericht";
+  }  
+                               (function () {
+    if (!window.chatbase || window.chatbase('getState') !== 'initialized') {
+        window.chatbase = (...args) => {
+            if (!window.chatbase.q) window.chatbase.q = [];
+            window.chatbase.q.push(args);
+        };
+        window.chatbase = new Proxy(window.chatbase, {
+            get(target, prop) {
+                if (prop === 'q') return target.q;
+                return (...args) => target(prop, ...args);
+            }
+        });
+    }
+    const onLoad = function () {
+        const script = document.createElement('script');
+        script.src = 'https://www.chatbase.co/embed.min.js';
+        script.id = 'C60jEJW_QuVD7X3vE5rzE';
+        script.setAttribute('domain', 'www.chatbase.co');
+        document.body.appendChild(script);
+    };
+    if (document.readyState === 'complete') onLoad();
+    else window.addEventListener('load', onLoad);
+})();
+
   });
 });
+
 
 
 
